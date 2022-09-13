c26bbb35c9ff25ed8579dada31ca05bea37bea82
fvn70 <fvn_reg@mail.ru>
2022-09-13 21:53:44 +0700

    Stage 3/9: Recursive splitting

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -1,82 +1,95 @@
 import pandas as pd
 
-def gi(a, b):
-    p1 = a / (a + b)
-    p2 = b / (a + b)
-    return 1 - p1**2 - p2**2
+class Node:
 
-def gini(node):
-    n = len(node)
-    p0 = node.count('0') / n
-    p1 = node.count('1') / n
-    g = 1 - p0 ** 2 - p1 ** 2
-    return g
+  def __init__(self):
+    # class initialization
+    self.left = None
+    self.right = None
+    self.term = False
+    self.label = None
+    self.feature = None
+    self.value = None
 
-def gini_w(node1, node2):
-    n1 = len(node1)
-    n2 = len(node2)
-    wg = (n1 * gini(node1) + n2 * gini(node2)) / (n1 + n2)
+  def set_split(self, feature, value):
+    # this function saves the node splitting feature and its value
+    self.feature = feature
+    self.value = value
+
+  def set_term(self, label):
+    # if the node is a leaf, this function saves its label
+    self.term = True
+    self.label = label
+
+  def __str__(self):
+      if self.term:
+          return f'Leaf = {self.label}'
+      else:
+          return f'Node split by {self.feature} = {self.value}:\n {self.left} {self.right}'
+
+def gini(lst):
+    n = len(lst)
+    vals = set(lst)
+    sum = 0
+    for v in vals:
+        sum += (lst.count(v) / n) ** 2
+    return 1 - sum
+
+def gini_w(lst1, lst2):
+    n1 = len(lst1)
+    n2 = len(lst2)
+    wg = (n1 * gini(lst1) + n2 * gini(lst2)) / (n1 + n2)
     return wg
 
-def cnt(cat, tag, v):
-    n, n0, n1 = 0, 0, 0
-    for i in range(len(cat)):
-        if cat[i] == v:
-            n += 1
-            if tag[i] == 0:
-                n0 += 1
-            else:
-                n1 += 1
-    return n, n0, n1
+def is_leaf(X, y):
+    if X.shape[0] <= 1 or gini(y.to_list()) == 0:
+        return True
+    return all(map(lambda x: len(set(x)) == 1, X.values.T))
 
-def gini_cat(cat, tag):
-    num = len(cat)
-    vals = set(cat)
-    g_w = 0
+def chose_split(X, y):
     g_min = 1
-    v_min = 0
-    for v in vals:
-        n, n0, n1 = cnt(cat, tag, v)
-        g = gi(n0, n1)
-        if g < g_min:
-            g_min = g
-            v_min = v
-        g_w += g * n / num
-    # print('g_w, v_min=', g_w, v_min)
-    return g_w, v_min
+    feature = None
+    f_value = None
+    split_1, split_2 = None, None
+    for col_name, values in X.iteritems():
+        vals = values.unique()
+        for v in vals:
+            idx_1 = X.index[X[col_name] == v].tolist()
+            idx_2 = X.index[X[col_name] != v].tolist()
+            wg = gini_w(y.iloc[idx_1].tolist(), y.iloc[idx_2].tolist())
+            if wg < g_min:
+                g_min = wg
+                feature = col_name
+                f_value = v
+                split_1, split_2 = idx_1, idx_2
+    return feature, f_value, split_1, split_2
 
-def split(nod, v):
-    nod1 = [i for i in range(len(nod)) if nod[i] == v]
-    nod2 = [i for i in range(len(nod)) if nod[i] != v]
-    return nod1, nod2
+def split_df(node, X, y):
+    if is_leaf(X, y):
+        node.set_term(y[0])
+        return
+    feature, f_value, split_1, split_2 = chose_split(X, y)
+    node.set_split(feature, f_value)
+    print(f'Made split: {node.feature} is {node.value}')
 
-def stage1():
-    node = input().split(' ')
-    node1 = input().split(' ')
-    node2 = input().split(' ')
-    g1 = gini(node)
-    g2 = gini_w(node1, node2)
-    print(round(g1, 2), round(g2, 2))
+    node.left = Node()
+    node.right = Node()
 
-def stage2():
+    left_X = X.iloc[split_1].reset_index(drop=True)
+    right_X = X.iloc[split_2].reset_index(drop=True)
+    left_y = y.iloc[split_1].reset_index(drop=True)
+    right_y = y.iloc[split_2].reset_index(drop=True)
+
+    split_df(node.left, left_X, left_y)
+    split_df(node.right, right_X, right_y)
+
+def stage3():
     fn = input()
-    # fn = 'test/data_stage2.csv'
-    df = pd.read_csv(fn)
-    tag = df['Survived']
-    g_min = 1
-    v_min = 0
-    i_min = 0
-    for i in range(1, df.shape[1] - 1):
-        cat = df.iloc[:, i]
-        g, v = gini_cat(cat, tag)
-        if g < g_min:
-            g_min = g
-            v_min = v
-            i_min = i
-    nod = df.iloc[:, i_min]
-    nod1, nod2 = split(nod, v_min)
-    # print('G=', round(g_min, 4), 'cat=', df.columns[i_min], 'v=', v_min)
-    print(round(g_min, 4), df.columns[i_min], v_min, nod1, nod2)
+    # fn = 'test/data_stage3.csv'
+    df = pd.read_csv(fn, index_col=0)
+    X = df.iloc[:, :-1]
+    y = df['Survived']
+    root = Node()
+    split_df(root, X, y)
 
-
-stage2()
+stage3()
