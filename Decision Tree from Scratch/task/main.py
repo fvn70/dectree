052f12ef66a32983b93e044e40804b34ff1e085d
fvn70 <fvn_reg@mail.ru>
2022-09-14 23:33:08 +0700

    Stage 4/9: Decision Tree

--- Decision Tree from Scratch/task/main.py
+++ Decision Tree from Scratch/task/main.py
@@ -1,5 +1,66 @@
 import pandas as pd
 
+class DecisionTree:
+    def __init__(self, root):
+        self.root = root
+        self.leaf_size = 1
+
+    def _gini(self, lst: list):
+        n = len(lst)
+        vals = set(lst)
+        sum = 0
+        for v in vals:
+            sum += (lst.count(v) / n) ** 2
+        return 1 - sum
+
+    def _gini_w(self, lst1, lst2):
+        n1 = len(lst1)
+        n2 = len(lst2)
+        wg = (n1 * self._gini(lst1) + n2 * self._gini(lst2)) / (n1 + n2)
+        return wg
+
+    def _is_leaf(self, X, y):
+        if X.shape[0] <= 1 or self._gini(y.to_list()) == 0:
+            return True
+        return all(map(lambda x: len(set(x)) == 1, X.values.T))
+
+    def _chose_split(self, X, y):
+        g_min = 1
+        feature = None
+        f_value = None
+        split_1, split_2 = None, None
+        for col_name, values in X.iteritems():
+            vals = values.unique()
+            for v in vals:
+                idx_1 = X.index[X[col_name] == v].tolist()
+                idx_2 = X.index[X[col_name] != v].tolist()
+                wg = self._gini_w(y.iloc[idx_1].tolist(), y.iloc[idx_2].tolist())
+                if wg < g_min:
+                    g_min = wg
+                    feature = col_name
+                    f_value = v
+                    split_1, split_2 = idx_1, idx_2
+        return feature, f_value, split_1, split_2
+
+    def fit(self, node, X, y):
+        if self._is_leaf(X, y):
+            node.set_term(y[0])
+            return
+        feature, f_value, split_1, split_2 = self._chose_split(X, y)
+        node.set_split(feature, f_value)
+        print(f'Made split: {node.feature} is {node.value}')
+
+        node.left = Node()
+        node.right = Node()
+
+        left_X = X.iloc[split_1].reset_index(drop=True)
+        right_X = X.iloc[split_2].reset_index(drop=True)
+        left_y = y.iloc[split_1].reset_index(drop=True)
+        right_y = y.iloc[split_2].reset_index(drop=True)
+
+        self.fit(node.left, left_X, left_y)
+        self.fit(node.right, right_X, right_y)
+
 class Node:
 
   def __init__(self):
@@ -27,69 +88,16 @@ class Node:
       else:
           return f'Node split by {self.feature} = {self.value}:\n {self.left} {self.right}'
 
-def gini(lst):
-    n = len(lst)
-    vals = set(lst)
-    sum = 0
-    for v in vals:
-        sum += (lst.count(v) / n) ** 2
-    return 1 - sum
 
-def gini_w(lst1, lst2):
-    n1 = len(lst1)
-    n2 = len(lst2)
-    wg = (n1 * gini(lst1) + n2 * gini(lst2)) / (n1 + n2)
-    return wg
 
-def is_leaf(X, y):
-    if X.shape[0] <= 1 or gini(y.to_list()) == 0:
-        return True
-    return all(map(lambda x: len(set(x)) == 1, X.values.T))
-
-def chose_split(X, y):
-    g_min = 1
-    feature = None
-    f_value = None
-    split_1, split_2 = None, None
-    for col_name, values in X.iteritems():
-        vals = values.unique()
-        for v in vals:
-            idx_1 = X.index[X[col_name] == v].tolist()
-            idx_2 = X.index[X[col_name] != v].tolist()
-            wg = gini_w(y.iloc[idx_1].tolist(), y.iloc[idx_2].tolist())
-            if wg < g_min:
-                g_min = wg
-                feature = col_name
-                f_value = v
-                split_1, split_2 = idx_1, idx_2
-    return feature, f_value, split_1, split_2
-
-def split_df(node, X, y):
-    if is_leaf(X, y):
-        node.set_term(y[0])
-        return
-    feature, f_value, split_1, split_2 = chose_split(X, y)
-    node.set_split(feature, f_value)
-    print(f'Made split: {node.feature} is {node.value}')
-
-    node.left = Node()
-    node.right = Node()
-
-    left_X = X.iloc[split_1].reset_index(drop=True)
-    right_X = X.iloc[split_2].reset_index(drop=True)
-    left_y = y.iloc[split_1].reset_index(drop=True)
-    right_y = y.iloc[split_2].reset_index(drop=True)
-
-    split_df(node.left, left_X, left_y)
-    split_df(node.right, right_X, right_y)
-
-def stage3():
+def stage4():
     fn = input()
-    # fn = 'test/data_stage3.csv'
+    # fn = 'test/data_stage4.csv'
     df = pd.read_csv(fn, index_col=0)
     X = df.iloc[:, :-1]
     y = df['Survived']
     root = Node()
-    split_df(root, X, y)
+    tree = DecisionTree(root)
+    tree.fit(root, X, y)
 
-stage3()
+stage4()
